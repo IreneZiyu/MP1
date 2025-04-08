3d7eab61f4968fa614ebdc4eead2afbc72a58dbd
IreneZiyu <912152409@qq.com>
2025-04-08 19:03:58 +0800

    Update education.ts

--- src/config/education.ts
+++ src/config/education.ts
@@ -16,21 +16,23 @@ export type EducationItemType = {
     {
       school: '帝国理工学院',
       major: '交通经济学',
+      department: '土木工程与环境学院',  
       logo: 'college',
-        department: '土木工程与环境学院',
       start: '2020',
       end: '2021'
     },
     {
-      school: 'University of Chicago',
-      major: 'Computer Science',
+      school: '利物浦大学',
+      major: '经济学',
+      department: '商学院', 
       logo: 'college',
-      start: '2013',
-      end: '2015'
+      start: '2018',
+      end: '2020'
     },
     {
       school: 'BUPT',
       major: 'Automation',
+      department: '商学院',
       logo: 'college',
       start: '2009',
       end: '2013'
